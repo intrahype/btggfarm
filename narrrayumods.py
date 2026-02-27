d4d495ade613640c182cd79b6410e004b9b97704
Kenyon Fryman <54225943+intrahype@users.noreply.github.com>
2026-02-26 20:26:50 -0800

    begin adding sqlite db

--- narrrayumods.py
+++ narrrayumods.py
@@ -1,6 +1,33 @@
 from flask import Flask
+import os.path
+from flask_sqlalchemy import SQLAlchemy
 
+# database SQLite
+db = SQLAlchemy()
+
+# start app
 app = Flask(__name__)
+
+# db name
+db_name = 'mod_list.db'
+
+# path to db
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, db_name)
+
+app.config['SQLALchemy_DATABASE_URI'] = 'sqlite:///' + db_path
+
+app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = True
+
+# initialize app with SQLAlchemy
+db.init_app(app)
+
+# database Class
+class Mods(db.Model):
+    __tablename__ = 'Stardew Valley Mods'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    last_updated = db.Column(db.String)
 
 @app.route('/')
 def index():
@@ -14,3 +41,22 @@ def user(name):
 def swdadjoke():
     return '<h1>Why did Princess Leia change her hair on Hoth?</h1> <b> insert pause </b> <h1> She was freezing her Buns off!</h1>'
 
+@app.route('/modlist')
+def modlist():
+    try:
+        mod_list = db.session.execute(db.select(Mods))
+
+        mod_text = '<ul>'
+        for mod in mod_list:
+            mod_text += '<li>' + mod_list.name + ', ' + mod_list.lastupdated + '</li>'
+    
+    except Exception as error:
+        #user error to hold error description
+        error_text = "<p>The error:<br>" + str(error) + "</p>"
+        header_return = '<h1>Something has broken.</h1>'
+        return header_return + error_text
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
